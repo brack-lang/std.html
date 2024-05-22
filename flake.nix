dcb7cf3be98f60a43e73f716c4d64263823b71dd
Mutsuha Asada <me@momee.mt>
2024-05-22 14:37:25 +0900

    updated flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -1,78 +1,74 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.11";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    naersk = {
-      url = "github:nix-community/naersk";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
-  outputs = { self, nixpkgs, fenix, naersk, flake-utils, flake-compat, ... }:
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        overlays = [
-          (_: super: let pkgs = fenix.inputs.nixpkgs.legacyPackages.${super.system}; in fenix.overlays.default pkgs pkgs)
-        ];
         pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        naersk' = pkgs.callPackage naersk {};
-        extism-cli = (import ./nix/extism-cli.nix {
-          buildGoModule = pkgs.buildGoModule;
-          fetchFromGitHub = pkgs.fetchFromGitHub;
-        });
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nil
-            openssl
-            glib
-            pkg-config
-            rustup
-            cargo
-            gcc
-            extism-cli
+          inherit system;
+          overlays = [
+            (import rust-overlay)
           ];
         };
-        packages.default =
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            target = "wasm32-unknown-unknown";
-            toolchain = with fenix.packages.${system}; combine [
-              minimal.cargo
-              minimal.rustc
-              targets.${target}.latest.rust-std
-            ];
-            wasmPackage = (naersk.lib.${system}.override {
-              cargo = toolchain;
-              rustc = toolchain;
-            }).buildPackage {
-              src = ./.;
-              copyLibs = true;
-              CARGO_BUILD_TARGET = target;
-              CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "${pkgs.llvmPackages.lld}/bin/lld";
-              postBuil = ''
-                for file in $(find $out -name "*.wasm"); do
-                  base=$(basename $file .wasm)
-                  renamed=$(echo $base | sed 's/_/./g').wasm
-                  mv $file $(dirname $file)/$renamed
-                done
-              '';
+        target = "wasm32-unknown-unknown";
+        toolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [target];
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = toolchain;
+          cargo = toolchain;
+        };
+        extism-cli = import ./nix/extism-cli.nix {
+          buildGoModule = pkgs.buildGoModule;
+          fetchFromGitHub = pkgs.fetchFromGitHub;
+        };
+      in {
+        packages.default = let
+          name = "std-html";
+          wasmPackage = rustPlatform.buildRustPackage {
+            inherit name;
+            src = ./.;
+            copyLibs = true;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "brack-sdk-rs-0.1.0" = "sha256-brLwtHdMiJytb/yzA857c508ZnA6OcKhHfGA6sxNITE=";
+              };
             };
-          in pkgs.stdenv.mkDerivation {
+            CARGO_BUILD_TARGET = target;
+            CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "${pkgs.llvmPackages.lld}/bin/lld";
+            postBuild = ''
+              for file in $(find $out -name "*.wasm"); do
+                base=$(basename $file .wasm)
+                renamed=$(echo $base | sed 's/_/./g').wasm
+                mv $file $(dirname $file)/$renamed
+              done
+            '';
+            buildPhase = ''
+              cargo build --release -p ${name} --target=${target}
+            '';
+            installPhase = ''
+              mkdir -p $out/lib
+              cp target/${target}/release/*.wasm $out/lib/
+            '';
+          };
+        in
+          pkgs.stdenv.mkDerivation {
             name = "brack-std-html";
-            buildInputs = [ pkgs.rename ];
-            
+            buildInputs = [pkgs.rename];
             inherit (wasmPackage) src;
-            
             installPhase = ''
               mkdir -p $out
               cp -r ${wasmPackage}/lib/* $out/
@@ -81,6 +77,15 @@
               done
             '';
           };
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            toolchain
+            rust-analyzer
+            nil
+            alejandra
+            extism-cli
+          ];
+        };
       }
     );
 }
