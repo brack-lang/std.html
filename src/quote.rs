b06b2452e3fd31f3faa95a16b0ae944a1e76e301
Mutsuha Asada <me@momee.mt>
2024-06-12 10:36:11 +0900

    add: inline quote

---
+++ src/quote.rs
@@ -0,0 +1,48 @@
+use brack_sdk_rs::{MetaData, Type, Value};
+use extism_pdk::{plugin_fn, FnResult, Json, WithReturnCode};
+
+#[plugin_fn]
+pub fn metadata_inline_quote() -> FnResult<Json<MetaData>> {
+    Ok(Json(MetaData {
+        command_name: ">".to_string(),
+        call_name: "inline_quote".to_string(),
+        argument_types: vec![("text".to_string(), Type::TInline)],
+        return_type: Type::TInline,
+    }))
+}
+
+#[plugin_fn]
+pub fn inline_quote(Json(args): Json<Vec<Value>>) -> FnResult<String> {
+    if args.len() != 2 {
+        return Err(WithReturnCode::new(
+            anyhow::anyhow!(
+                "Usage:
+    1. [std.> text]
+    2. [std.> text, cite]"
+            ),
+            1,
+        ));
+    }
+    let text = match &args[0] {
+        Value::Text(t) => t,
+        _ => {
+            return Err(WithReturnCode::new(
+                anyhow::anyhow!("text must be Value::Text"),
+                1,
+            ))
+        }
+    };
+    let cite = match &args[1] {
+        Value::TextOption(t) => t,
+        _ => {
+            return Err(WithReturnCode::new(
+                anyhow::anyhow!("text must be Value::TextOption"),
+                1,
+            ))
+        }
+    };
+    match cite {
+        Some(cite) => Ok(format!("<q cite=\"{}\">{}</q>", cite, text)),
+        None => Ok(format!("<q>{}</q>", text)),
+    }
+}
