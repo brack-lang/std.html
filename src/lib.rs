644e640ef738171a4be98603f7ac2bae61243758
Mutsuha Asada <me@momee.mt>
2024-06-13 09:11:19 +0900

    add: strike

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,4 @@ pub mod headings;
 pub mod italic;
 pub mod list;
 pub mod quote;
+pub mod strike;
---
+++ src/strike.rs
@@ -0,0 +1,29 @@
+use brack_sdk_rs::{MetaData, Type, Value};
+use extism_pdk::{plugin_fn, FnResult, Json, WithReturnCode};
+
+#[plugin_fn]
+pub fn metadata_strike() -> FnResult<Json<MetaData>> {
+    Ok(Json(MetaData {
+        command_name: "~".to_string(),
+        call_name: "strike".to_string(),
+        argument_types: vec![("text".to_string(), Type::TInline)],
+        return_type: Type::TInline,
+    }))
+}
+
+#[plugin_fn]
+pub fn strike(Json(args): Json<Vec<Value>>) -> FnResult<String> {
+    if args.len() != 1 {
+        return Err(WithReturnCode::new(anyhow::anyhow!("Usage: [~ text]"), 1));
+    }
+    let text = match &args[0] {
+        Value::Text(t) => t,
+        _ => {
+            return Err(WithReturnCode::new(
+                anyhow::anyhow!("text must be Value::Text"),
+                1,
+            ))
+        }
+    };
+    Ok(format!("<s>{}</s>", text))
+}
